2ab6a44aecb66a2df4575333477e83cf5ce3feea
Zhenhui Chen <zhenhuiccc@gmail.com>
2020-02-13 23:07:29 +0800

    项目结构创建

---
+++ gmall-ums/src/test/java/com/atguigu/gmall/lums/GmallUmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.lums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallUmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
